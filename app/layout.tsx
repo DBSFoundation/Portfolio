64b97be252c4e1f63d837181e63131971340f95c
Dandy DS <134028652+DBSFoundation@users.noreply.github.com>
2025-08-26 00:34:59 +0700

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Premilyader | Portfolio",
+  title: "Dandy DS | Portfolio",
   description: "Crypto Trader, Full-Stack Developer & Airdrop Hunter",
 };
 
